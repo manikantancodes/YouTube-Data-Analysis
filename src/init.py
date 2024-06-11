0c87c5a3fa5b2dc90c6f6367f39228790d9253a9
Mandal Manikantan <125236242+manikantancodes@users.noreply.github.com>
2024-06-11 13:33:24 +0530

    Update init.py

--- src/init.py
+++ src/init.py
@@ -1,33 +1,43 @@
 # src/__init__.py
 
-# Import modules from the project
-from youtubehv import (
-    Api_connect,
-    get_channel_info,
-    get_videos_ids,
-    get_video_info,
-    get_comment_info,
-    get_playlist_details,
-    channels_table,
-    playlist_table,
-    videos_table,
-    comments_table,
-    tables,
-    connect_to_sql,
-    show_channels_table,
-    show_playlists_table,
-    show_videos_table,
-    show_comments_table,
-    execute_question,
-)
+# Importing all modules from the project
+
+from .api_connect import api_connect
+from .get_channel_info import get_channel_info
+from .get_videos_ids import get_videos_ids
+from .get_video_info import get_video_info
+from .get_comment_info import get_comment_info
+from .get_playlist_details import get_playlist_details
+from .create_and_insert_table import create_and_insert_table
+from .channels_table import channels_table
+from .playlist_table import playlist_table
+from .videos_table import videos_table
+from .comments_table import comments_table
+from .tables import tables
+from .connect_to_sql import connect_to_sql
+from .show_channels_table import show_channels_table
+from .show_playlists_table import show_playlists_table
+from .show_videos_table import show_videos_table
+from .show_comments_table import show_comments_table
+from .visualize_subscribers import visualize_subscribers
+from .visualize_top_videos import visualize_top_videos
+from .visualize_video_duration import visualize_video_duration
+from .visualize_video_duration_scatter import visualize_video_duration_scatter
+from .visualize_videos_per_channel import visualize_videos_per_channel
+from .visualize_avg_views_per_channel import visualize_avg_views_per_channel
+from .visualize_video_definitions import visualize_video_definitions
+from .execute_question import execute_question
+from .show_individual_channel_details import show_individual_channel_details
+from .main import main
 
 __all__ = [
-    "Api_connect",
+    "api_connect",
     "get_channel_info",
     "get_videos_ids",
     "get_video_info",
     "get_comment_info",
     "get_playlist_details",
+    "create_and_insert_table",
     "channels_table",
     "playlist_table",
     "videos_table",
@@ -38,5 +48,14 @@ __all__ = [
     "show_playlists_table",
     "show_videos_table",
     "show_comments_table",
+    "visualize_subscribers",
+    "visualize_top_videos",
+    "visualize_video_duration",
+    "visualize_video_duration_scatter",
+    "visualize_videos_per_channel",
+    "visualize_avg_views_per_channel",
+    "visualize_video_definitions",
     "execute_question",
+    "show_individual_channel_details",
+    "main"
 ]
